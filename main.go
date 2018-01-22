dca914e2aec920964cc7c454aedff4dc3ecf3db4
Matt Vinall <mattv@unity3d.com>
2018-01-22 10:56:42 +0000

    add metadata update

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 )
 
 func recreateBucket(minioClient *minio.Client) {
+	fmt.Println("--------recreate bucket-------------")
 	list(minioClient, func(object minio.ObjectInfo) {
 		fmt.Println("deleting", object.Key)
 
@@ -34,6 +35,7 @@ func recreateBucket(minioClient *minio.Client) {
 }
 
 func put(minioClient *minio.Client) {
+	fmt.Println("--------put-------------")
 	file, err := os.Open("Makefile")
 	if err != nil {
 		fmt.Println(err)
@@ -61,6 +63,7 @@ func put(minioClient *minio.Client) {
 }
 
 func list(minioClient *minio.Client, do func(minio.ObjectInfo)) {
+	fmt.Println("--------list-------------")
 	// Create a done channel to control 'ListObjectsV2' go routine.
 	doneCh := make(chan struct{})
 
@@ -79,12 +82,31 @@ func list(minioClient *minio.Client, do func(minio.ObjectInfo)) {
 }
 
 func statobject(minioClient *minio.Client) {
+	fmt.Println("--------stat-------------")
 	i, err := minioClient.StatObject("mybucket", "myobject", minio.StatObjectOptions{})
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Printf("stat %+v\n", i)
+}
+
+func updateMetadata(minioClient *minio.Client) {
+	fmt.Println("--------update-------------")
+	dest, err := minio.NewDestinationInfo("mybucket", "myobject", nil, map[string]string{"foo": "bar"})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	src := minio.NewSourceInfo("mybucket", "myobject", nil)
+
+	err = minioClient.CopyObject(dest, src)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Updated metadata\n")
 }
 
 func main() {
@@ -98,9 +120,16 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	minioClient.SetAppInfo("s3client", "0.0.1 dev")
+	// minioClient.TraceOn(os.Stdout)
 
 	recreateBucket(minioClient)
 	put(minioClient)
+	list(minioClient, func(object minio.ObjectInfo) {
+		fmt.Printf("%+v\n", object)
+	})
+	statobject(minioClient)
+	updateMetadata(minioClient)
 	list(minioClient, func(object minio.ObjectInfo) {
 		fmt.Printf("%+v\n", object)
 	})
